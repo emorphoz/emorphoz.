b07f0aaff86b0731bdc00ecbfd448574201bc002
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-10-19 10:49:31 +0200

    Ajout Ensemble des vues et organisations des pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -12,7 +11,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About Us</RouterLink>
+        <RouterLink to="/how">How ?</RouterLink>
+        <RouterLink to="/mentionslegales">Mentions Légales</RouterLink>
+        <RouterLink to="/market">Market Place</RouterLink>
+        <RouterLink to="/ladybugpresent">Ladybug Présentation</RouterLink>
+        <RouterLink to="/ladybuggenerate">Ladybug Générer</RouterLink>
+        <RouterLink to="/pay">Payement</RouterLink>
+        <RouterLink to="/login">Connexion</RouterLink>
+        <RouterLink to="/sign">Inscription</RouterLink>
+
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '../pages/HomeView.vue'
+import AboutView from '../pages/AboutView.vue'
+import MentionsLegalesView from '../pages/MentionsLegalesView.vue'
+import HowView from '../pages/HowView.vue'
+
+import PayView from '../pages/PayView.vue'
+
+import LoginView from '../pages/Accounts/LoginView.vue'
+import SignView from '../pages/Accounts/SignView.vue'
+
+// MARKETPLACE
+import MarketView from '../pages/MarketPlace/index.vue'
+
+    // Ladybug
+    import LadybugPresentView from '../pages/MarketPlace/Ladybug/index.vue' 
+    import LadybugGenerateView from '../pages/MarketPlace/Ladybug/generate.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    {path: '/', name: 'home', component: HomeView},
+    {path: '/about', name: 'about', component: AboutView},
+    {path: '/how', name: 'how', component: HowView},
+    {path: '/mentionslegales', name: 'mentionslegales', component: MentionsLegalesView},
+    {path: '/pay', name: 'pay', component: PayView},
+
+    {path: '/login', name: 'login', component: LoginView},
+    {path: '/sign', name: 'sign', component: SignView},
+    // {path: '/sign', name: 'sign', component: SignView}, -----> Configuration Profil
+
+    {path: '/market', name: 'market', component: MarketView},
+
+    {path: '/ladybugpresent', name: 'ladybugpresent', component: LadybugPresentView},
+    {path: '/ladybuggenerate', name: 'ladybuggenerate', component: LadybugGenerateView},
   ]
 })
 
