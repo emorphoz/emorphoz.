54b12fc0a2d97e2f30af8637f225b82cff75b2f4
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-11-15 22:34:56 +0100

    Modification / Tentative Réparation class?

--- src/App.vue
+++ src/App.vue
@@ -3,91 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<div class="bg-epurple">
+    fdshgjdfhgjfdhghdsjfhsdfsd
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <img src="../public/img/iconemorphoz.gif" alt="" class="animate-spin">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About Us</RouterLink>
-        <RouterLink to="/how">How ?</RouterLink>
-        <RouterLink to="/mentionslegales">Mentions Légales</RouterLink>
-        <RouterLink to="/market">Market Place</RouterLink>
-        <RouterLink to="/ladybugpresent">Ladybug Présentation</RouterLink>
-        <RouterLink to="/ladybuggenerate">Ladybug Générer</RouterLink>
-        <RouterLink to="/pay">Payement</RouterLink>
-        <RouterLink to="/login">Connexion</RouterLink>
-        <RouterLink to="/sign">Inscription</RouterLink>
-
-      </nav>
-    </div>
-  </header>
+  <h1 class="pt-10">emorphoz</h1>
 
   <RouterView />
+
+</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2 class="font-audiowide bg-epurple">test</h2>
+        <Router-Link to="/about"><h3>test</h3></Router-Link>
+    </div>
+</template>
