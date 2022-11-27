2822f3df2adc41b87ce74ee472965981a90c8b1e
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-11-27 21:45:05 +0100

    Soon Finish HomePage

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,12 +2,128 @@
 
 <div>
     <img src="/public/icon/emorphoz_long.svg" alt="" class="w-96">
-    <h1 class="font-audiowide text-ewhite">invest in yourself</h1>
-    <div class="flex">
-        <p class="text-ewhite font-trap-semibold">in <span class="text-eblue">collaboration</span> with the</p>
-        <img src="/public/icon/OFFF.svg" alt="" class="w-20">
+    <h1 class="font-audiowide text-ewhite ml-10 text-lg">invest in yourself</h1>
+    <div class="flex ml-10">
+        <p class="text-ewhite font-trap-semibold text-base">in <span class="text-eblue">collaboration</span> with the</p>
+        <a href="https://www.offf.barcelona/"><img src="/public/icon/OFFF.svg" alt=""></a>
     </div>
-    <img src="/public/img/3-Ellipse.png" class="absolute top-36 right-0">
-    <img src="/public/icon/orbe.svg" class="absolute scale-150 top-36 right-0">
+    <img src="/public/img/3-Ellipse.png" class="absolute top-44 right-0 w-1/5 z-10">
+    <img src="/public/icon/orbe-purple.svg" class="absolute scale-150 top-36 right-14 z-10">
+
+    <img src="/public/img/Ellipse-lineshort.png" alt="" class="absolute left-96 w-1/5 z-10">
+
+    <div class="w-1/2 z-20">
+    <p class="text-ewhite font-trap-regular pt-40 ml-10 text-base z-20">Discover a unique generator that will create a unique <br />
+    work designed specifically for your tastes but also <br />
+    according to the ambient temperature of your city...</p>
+    <img src="/public/img/Ellipse-lineshort.png" alt="" class="absolute z-10 w-1/6 -bottom-24">
+
+    <img src="/public/img/coccinelle.png" alt="" class="z-20 absolute -bottom-24 w-1/3 right-0">
+    <img src="/public/img/nuage.png" alt="" class="z-10 absolute -bottom-24 w-1/3 right-0 opacity-30 epur">
+    <img src="/public/icon/orbe-purple.svg" class="absolute scale-150 right-72 bottom-20 z-0 rotate-90 w-1/6">
+
+    <div class="grid grid-cols-3 content-between mt-5">
+        <div class="col-span-1"></div>
+        <Router-Link to="/market" class="z-20"><button class="bg-eblue shadow-turquoizeBlur bg-opacity-80 border-2 rounded-xl border-eblue p-2"><p class="font-trap-regular text-ewhite text-lg z-20">Discover</p></button></Router-Link>
+    </div>
+    </div>
+
+    <img src="/public/icon/orbe-pink.svg" alt="" class="absolute z-10 -bottom-96 w-1/4">
+
+    <!-- <img src="/public/" alt=""> == Gif System         -->
+
+    <div class="flex">
+        <img src="/public/img/NFTsymb.png" alt="" class="h-28 -bottom-96 absolute">
+        <div>
+            <p class="font-trap-semibold text-ewhite text-lg mt-52 desktop:ml-16">300, 000 NFTs</p> 
+            <p class="text-ewhite text-opacity-75 font-trap-light text-base text-center">Now available</p>
+        </div>
+    </div>
+
+    <img src="/public/icon/orbe-purple.svg" alt="" class="absolute z-10 w-1/3 rotate-180 -left-44">
+    <img src="/public/icon/orbe-pink.svg" alt="" class="absolute z-10 w-1/5 right-0 rotate-180">
+
+    <div class="grid grid-cols-6 gap-10 mt-24">
+        <div class="col-span-1"></div>
+            <div class="col-span-4 row" >
+                <p class="text-ewhite font-trap-semibold text-base">When you generate an nft, a smart contract is made between 4 differents parties:</p>
+            </div>
+        <div class="col-span-1"></div>
+        <div class="col-span-1"></div>
+
+            <div class="col-span-1">
+                <img src="/public/icon/User.svg" alt="">
+                <p class="text-ewhite font-trap-light text-xs">You</p>
+            </div>
+
+            <div class="col-span-1">
+                <img src="/public/icon/People.svg" alt="">
+                <p class="text-ewhite font-trap-light text-xs">An association</p>
+            </div>
+
+            <div class="col-span-1">
+                <img src="/public/icon/emorphoz_long.svg" alt="">
+                <p class="text-ewhite font-trap-light text-xs">E-morphoz</p>
+            </div>
+
+            <div class="col-span-1">
+                <img src="/public/icon/OFFF.svg" alt="">
+                <p class="text-ewhite font-trap-light text-xs">The OFFF festival</p>
+            </div>
+
+        <div class="col-span-1"></div>
+        <div class="col-span-1"></div>
+
+        <div class="col-span-4">
+            <p class="text-ewhite font-trap-light text-xs">
+                This smart contract states that once the NFT is entered in the blockchain , <span class="text-eblue">8% of the selling
+                price will be given to the 4 original parties</span>, forever. If you choose to invest with E-morphoz, you
+                will receive 2% of each resale in addition to participate in a donation for an association 
+            </p>
+        </div>
+
+    </div>
+
+    <div class="grid grid-cols-3 mt-20">
+        <div class="col-span-1"></div>
+
+        <div class="col-span-1">
+            <p class="text-ewhite text-base font-audiowide absolute z-50 left-96 desktop:ml-20">Many textures available</p>
+            <Router-Link to="/market"><button class="absolute desktop:mt-10 desktop:ml-24 scale-50 bg-eblue shadow-turquoizeBlur bg-opacity-80 border-2 rounded-xl border-eblue p-2 z-20"><p class="font-trap-regular text-ewhite text-lg z-20">Generate now</p></button></Router-Link>
+            <img src="/public/img/CardTextu.png" alt="" class="z-10 scale-150">
+        </div>
+
+        <div class="col-span-1"></div>
+    </div>
+
+    <img src="/public/icon/orbe-purple.svg" alt="" class="absolute z-0 rotate-180 w-1/3 desktop:-mt-52">
+    <img src="/public/icon/orbe-pink.svg" alt="" class="absolute z-0 rotate-180 w-1/5 right-0">
+    <p class="text-center mt-20 text-ewhite font-audiowide text-lg z-20">A more <span class="text-eblue">eco-friendly</span> way to pay and store your NFT</p>
+
+    <div class="grid grid-cols-3 mt-10">
+        <div class="col-span-1"></div>
+        <a href="https://tezos.com/" target="_blank"><button class="bg-eblue shadow-turquoizeBlur bg-opacity-80 border-2 rounded-xl border-eblue p-2 z-20 desktop:ml-28"><img src="/public/img/tezosButton.png" alt=""></button></a>
+    </div>
+
+    <div class="grid grid-cols-2">
+        <img src="/public/img/carte.png" alt="">
+        
+        <div class="desktop:mt-72">
+            <h2 class="text-ewhite font-audiowide text-lg text-center">Why <span class="text-eblue"> Tezos </span> ?</h2>
+            <p class="text-ewhite font-trap-light text-light">Unlike blockchains such as Bitcoin or Etherum, the tezos system requires much less carbon energy and running costs making it an ideal alternative platform to create environmentally friendly blockchain applications</p>
+        </div>
+
+    </div>
+
+    <!-- <div class="grid grid-cols-3">
+        <div class="col-span-1"></div>
+
+
+
+    </div> -->
+
 </div>
 </template>
+
+<style scoped>
+</style>
