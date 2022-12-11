80aa6b2a53e6b6e97fda8493f67c414dd8a9084a
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-12-11 14:57:35 +0100

    Ajout Asso-AboutUs

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -10,7 +10,7 @@
 
     <section class="grid desktop:grid-cols-2 phone:grid-cols-1 mx-10 relative mt-8">
         <div class="col-span-1">
-            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink">Adobe substance 3D</h2>
+            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink mb-4">Adobe substance 3D</h2>
             <p class="text-ewhite phone:text-3xs desktop:text-xs font-trap-regular text-shadow-epurple">This suite allowed us to create unique and different textures for each model or accessories. <br /><br />
             Each object is then stagedto bring a touch of reality to the creation</p>
         </div>
@@ -28,7 +28,7 @@
         </div>
 
         <div class="col-span-1">
-            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink">Adobe Photoshop, illustrator and Blender</h2>
+            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink mb-4">Adobe Photoshop, illustrator and Blender</h2>
             <p class="text-ewhite phone:text-3xs desktop:text-xs font-trap-regular text-shadow-epurple">With these programs we first create the logo with illustrator and with Photoshop all the secondary elements of our creation such as background, effects with temperatures etc... <br/> <br/>
             For Blender it was used mainly for animation that explains how our NFT is creating</p>
         </div>
@@ -38,7 +38,7 @@
 
     <section class="grid desktop:grid-cols-2 phone:grid-cols-1 mx-10 relative mt-8">
         <div class="col-span-1">
-            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink">Visual Studio Code and the language Ligo</h2>
+            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink mb-4">Visual Studio Code and the language Ligo</h2>
             <p class="text-ewhite phone:text-3xs desktop:text-xs font-trap-regular text-shadow-epurple">With VScode we have developped our entire website using speciafically view.js and as a supabase database <br/> <br/>
             The Ligo language allowed us to understand how linked the Tezos cryptocurrency to our project</p>
         </div>
@@ -47,6 +47,21 @@
             <img src="/public/img/vs-code_ligo.png" alt="" class="desktop:h-[300px] desktop:flex hidden">
             <img src="/public/img/vs-code_ligo_phone.png" alt="" class="desktop:hidden flex">
         </div>
+    </section>
+
+    <section class="grid desktop:grid-cols-2 phone:grid-cols-1 mx-10 relative mt-20 mb-20">
+
+        <div class="desktop:-mt-16 justify-center col-span-1 desktop:flex hidden">
+            <img src="/public/img/opie.png" alt="" class="desktop:h-[500px]">
+        </div>
+
+        <div class="col-span-1">
+            <h2 class="text-ewhite text-center phone:text-xs desktop:text-base  font-audiowide text-shadow-epink mb-4">The association</h2>
+            <p class="text-ewhite phone:text-3xs desktop:text-xs font-trap-regular text-shadow-epurple">After some time to think about the association to wich we give 2% of each NFT resale, we chose OPIE. The "Office pour les insectes et leur environnement" is a French association created in 1969. It gathers amateur and professionnal naturalists by acting for the entomological knowledge, the protection of the environnements and the sensitization of all the public to the world of the insects. <br /> <br />
+                This choice of association is the most appropriate for our project. It goes hand in hand with our identity psotioned on insects. Thus, by investing in our NFT, you also participate in the protection of their environment.</p>
+        </div>
+
+        <img src="/public/img/suite-adobe-2_phone.png" alt="" class="desktop:hidden flex col-span-1">
     </section>
 
     
