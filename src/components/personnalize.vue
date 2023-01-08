13ca7297c670fa83e65fb0a3af63240d9172446d
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2023-01-08 15:05:25 +0100

    Correction responsive personnalize

--- src/components/personnalize.vue
+++ src/components/personnalize.vue
@@ -1,23 +1,27 @@
 <template>
 
-        <h1 class="text-center" >{{ cpChoose }}</h1>
+<h1 class="text-ewhite text-center phone:text-base desktop:text-lg font-audiowide text-shadow-epurple">Choose your shell</h1>
         <div class="flex justify-center">
 
 <button v-on:click="cpChoose -= 1; if(cpChoose < 1){cpChoose = 4}"><img src="/public/icon/slide_left.svg" alt=""></button>
 
 <div class="flex justify-center relative col-span-2">
-  <img :src="bg" alt="" class="mb-10 w-1/3 relative"/>        <!-- BG -->
-  <img :src="support_corps" alt="" class="absolute w-1/3">    <!-- SUP -->
-  <img :src="head" alt="" class="absolute w-1/3">             <!-- Head -->
-  <img src="/public/insects/ladybugs/carapaces/cp_lava.png" alt="" class="absolute w-1/3" v-if="cpChoose == 1">                <!-- Carap -->
-  <img src="/public/insects/ladybugs/carapaces/cp_ocean.png" alt="" class="absolute w-1/3" v-if="cpChoose == 2">               <!-- Carap -->
-  <img src="/public/insects/ladybugs/carapaces/cp_space.png" alt="" class="absolute w-1/3" v-if="cpChoose == 3">               <!-- Carap -->
-  <img src="/public/insects/ladybugs/carapaces/cp_nature.png" alt="" class="absolute w-1/3" v-if="cpChoose == 4">              <!-- Carap -->
+  <img :src="bg" alt="" class="mb-10 desktop:w-1/3 relative"/>        <!-- BG -->
+  <img :src="support_corps" alt="" class="absolute desktop:w-1/3">    <!-- SUP -->
+  <img :src="head" alt="" class="absolute desktop:w-1/3">             <!-- Head -->
+  <img src="/public/insects/ladybugs/carapaces/cp_lava.png" alt="" class="absolute desktop:w-1/3" v-if="cpChoose == 1">                <!-- Carap -->
+  <img src="/public/insects/ladybugs/carapaces/cp_ocean.png" alt="" class="absolute desktop:w-1/3" v-if="cpChoose == 2">               <!-- Carap -->
+  <img src="/public/insects/ladybugs/carapaces/cp_space.png" alt="" class="absolute desktop:w-1/3" v-if="cpChoose == 3">               <!-- Carap -->
+  <img src="/public/insects/ladybugs/carapaces/cp_nature.png" alt="" class="absolute desktop:w-1/3" v-if="cpChoose == 4">              <!-- Carap -->
 </div>
 
 <button v-on:click="cpChoose += 1; if(cpChoose > 4){cpChoose = 1}"><img src="/public/icon/slide_right.svg" alt=""></button>
 
 </div>  
+
+<div class="flex justify-center desktop:-ml-20 phone:-ml-20 mb-20">
+  <button class="bg-eblue shadow-turquoizeBlur bg-opacity-80 border-2 rounded-[20px] px-10 py-3 border-eblue z-40 phone:ml-20 desktop:w-1/4 mt-10"><p class="font-trap-regular text-ewhite text-base z-40">Validate</p></button>
+</div>
 </template>
 
 <script>
